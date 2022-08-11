bde68e3720977a07ff613357cfbe154d7da00b0e
pasadi <parna80as@gmail.com>
2022-08-11 17:46:32 +0430

    change ts

---
+++ main.js
@@ -0,0 +1,48 @@
+// description
+// Check both client and browser ips
+export async function checkVPN() {
+  let browser = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  return fetch(`https://ipapi.co/json`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let client = data.timezone;
+      console.warn(`browser timezone: ${browser}`, `ip timezone: ${client}`);
+      return {
+        browser: browser,
+        ip: data,
+        hasVPN: client != browser,
+      };
+    });
+}
+
+// description
+// Remove background & make it white
+
+export default async function RemoveImgBackground(APIkey, file, setFileObj) {
+  const formData = new FormData();
+
+  formData.append("size", "auto");
+  formData.append("bg_color", "fff");
+  formData.append("image_file", file, file.name);
+  console.log(file);
+  const res = await fetch("https://api.remove.bg/v1.0/removebg", {
+    headers: {
+      "X-Api-Key": APIkey,
+    },
+    method: "POST",
+    body: formData,
+  });
+  if (res.status < 400) {
+    try {
+      setFileObj(res);
+      return;
+    } catch (err) {
+      return true;
+    }
+  }
+  const error = await res.json();
+  throw error;
+}
